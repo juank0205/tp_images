bae335ae2a0271de7b13ce898b0ae06744354101
juank0205 <jkamiloeche@outlook.com>
2025-09-26 16:50:34 +0200

    bug images

--- stochastic/main.py
+++ stochastic/main.py
@@ -1,3 +1,4 @@
+from math import floor, sqrt
 import scipy.stats
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,9 +29,58 @@ def display_neyron(aggx, aggy, childx, childy):
     plt.plot(aggx, aggy, "+")
     plt.plot(childrenx, childreny, "+")
 
-aggx, aggy, childrenx, childreny = neyman_scott(5, 10, -10, 10, -10, 10, 2)
-display_neyron(aggx, aggy, childrenx, childreny)
+def display_gibbs(x, y):
+    plt.plot(x, y, "+")
+
+def energyFunction(d):
+    if d <= 15 and d > 0:
+        return 50
+    elif d <=30:
+        return -10
+    else:
+        return 0
+
+def euclideanDistance(x1, y1, x2, y2):
+    # print(x1, x2, y1, y2)
+    return sqrt((x1-x2)**2 + (y1-y2)**2)
+
+def energy(x, y, xk, yk, energyFunctionCallback):
+    total_energy = 0
+    for xi, yi in zip(x, y):
+        total_energy += energyFunctionCallback(euclideanDistance(xk, xi, yk, yi))
+    return total_energy
+
+def gibbs(iPoints, iterations, candidates):
+    x, y = cond_poisson(iPoints, -1000, 1000, -1000, 1000)
+    display_cond(x, y, "Original", 1, 2, 1)
+    print(len(x))
+    for _ in range(iterations):
+        chosen_one = floor(np.random.rand() * iPoints)
+        chosen_x, chosen_y = x[chosen_one], y[chosen_one]
+        min_energy = energy(x, y, chosen_x, chosen_y, energyFunction)
+        x = np.delete(x, chosen_one)
+        y = np.delete(y, chosen_one)
+        for _ in range(candidates):
+            candidate_x, candidate_y = cond_poisson(1, -1000, 1000, -1000, 1000) 
+            candidate_x = candidate_x[0]
+            candidate_y = candidate_y[0]
+            candidate_energy = energy(x, y, candidate_x, candidate_y, energyFunction)
+            if candidate_energy <= min_energy:
+                min_energy = candidate_energy
+                chosen_x = candidate_x
+                chosen_y = candidate_y
+        x = np.append(x, chosen_x)
+        y = np.append(y, chosen_y)
+
+    return x, y
+
+x, y = gibbs(1000, 1, 4)
+display_cond(x, y, "Gibbs", 1, 2, 2)
 plt.show()
+
+# aggx, aggy, childrenx, childreny = neyman_scott(5, 10, -10, 10, -10, 10, 2)
+# display_neyron(aggx, aggy, childrenx, childreny)
+# plt.show()
 
 # x, y = cond_poisson(200, -10, 10, -10, 10)
 # display_figures(x, y, "Distribution 1", 2, 2, 1)
